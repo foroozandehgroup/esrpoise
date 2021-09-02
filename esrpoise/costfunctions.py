813308af88f7e1bd09ead70a91fbac05cb31352e
JB-V <jean-baptiste.verstraete@spc.ox.ac.uk>
2021-09-02 11:46:15 +0100

    cost functions consisteny

--- esrpoise/costfunctions.py
+++ esrpoise/costfunctions.py
@@ -2,15 +2,13 @@
 costfunctions.py
 ----------------
 
-# TODO rename functions for more consistency
-
 Contains some typical cost functions. You can choose to take your cost
 functions from here, or create your own.
 
 Cost functions must be defined as follows. They must take one parameter (named
 anything you like), which is an instance of the XeprDataset class and
 represents a set of ESR data. Then they must return a float, which corresponds
-to how 'bad' the spectrum is. The worse the spectrum, the larger the return
+to how 'bad' the data is. The worse the data, the larger the return
 value should be.
 
 SPDX-License-Identifier: GPL-3.0-or-later
@@ -19,21 +17,23 @@ SPDX-License-Identifier: GPL-3.0-or-later
 import numpy as np
 
 
-# spectrum
+# spectrum.
+def spectrum(data):
+    return np.fft.fft(data.O.real + 1j * data.O.imag)
+
+
 def minabsint(data):
     """
-    Cost function which minimises the absolute (magnitude-mode) intensity of
-    the spectrum.
+    Minimises the absolute (magnitude-mode) intensity of the spectrum.
     """
-    return np.sum(np.abs(np.fft.fft(data.O.real + 1j * data.O.imag)))
+    return np.sum(spectrum(data))
 
 
 def maxabsint(data):
     """
-    Cost function which maximises the absolute (magnitude-mode) intensity of
-    the spectrum.
+    Maximises the absolute (magnitude-mode) intensity of the spectrum.
     """
-    return -np.max(np.abs(np.fft.fft(data.O.real + 1j * data.O.imag)))
+    return -minabsint(data)
 
 
 def minrealint(data):
@@ -41,15 +41,29 @@ def minrealint(data):
     Minimises the intensity of the real part of the spectrum. If the spectrum
     has negative peaks this cost function will try to maximise those.
     """
-    raise NotImplementedError
-    # return np.sum(np.fft(data.r))
+    return np.sum(np.real(spectrum(data)))
 
 
 def maxrealint(data):
     """
     Maximises the intensity of the real part of the spectrum.
     """
-    return -np.sum(np.real(np.fft.fft(data.O.real, 2 * len(data.O.real))))
+    return -minrealint(data)
+
+
+def minmagint(data):
+    """
+    Minimises the intensity of the imagignary part of the spectrum. If the
+    spectrum has negative peaks this cost function will try to maximise those.
+    """
+    return np.sum(np.imag(spectrum(data)))
+
+
+def maximagint(data):
+    """
+    Maximises the intensity of the real part of the spectrum.
+    """
+    return -minmagint(data)
 
 
 def zerorealint(data):
@@ -58,53 +72,106 @@ def zerorealint(data):
     possible. This works by summation, so dispersion-mode peaks will not
     contribute to this cost function (as they add to zero).
     """
-    raise NotImplementedError
-    # return np.abs(np.sum(np.fft(data.r)))
+    return np.abs(np.sum(np.fft(data.O.real)))
+
+
+def zeroimagint(data):
+    """
+    Tries to get the intensity of the imaginary spectrum to be as close to zero
+    as possible.
+    """
+    return np.abs(np.sum(np.fft(data.O.imag)))
+
+
+# echo
+def minabsint_echo(data):
+    """
+    Minimises the absolute (magnitude-mode) intensity of the echo.
+    """
+    return np.sum(np.abs(data.O.real + 1j * data.O.imag))
+
+
+def maxabsint_echo(data):
+    """
+    Maximises the absolute (magnitude-mode) intensity of the echo.
+    """
+    return -minabsint_echo(data)
+
+
+def minrealint_echo(data):
+    """
+    Minimises the maximum intensity of the real part of the echo.
+    """
+    return np.sum(data.O.real)
 
 
 def maxrealint_echo(data):
     """
     Maximises the maximum intensity of the real part of the echo.
     """
-    return -np.max(data.O.real)
+    return -minrealint_echo(data)
 
 
-# echo
-def maxabsint_echo(data):
+def minimagint_echo(data):
     """
-    Cost function which maximises the absolute (magnitude-mode) intensity of
-    an echo.
+    Minimises the maximum intensity of the imaginary part of the echo.
     """
-    return -np.max(np.abs(data.O.real + 1j * data.O.imag))
-
-
-def sumabsint_echo(data):
-    """
-    Cost function which maximises the absolute (magnitude-mode) intensity of
-    an echo.
-    """
-    return -np.sum(np.abs(data.O.real + 1j * data.O.imag))
-
-
-def sumrealint_echo(data):
-    """
-    Cost function which maximises the absolute (magnitude-mode) intensity of
-    an echo.
-    """
-    return -np.sum(np.abs(data.O.real))
+    return np.sum(data.O.imag)
 
 
 def maximagint_echo(data):
     """
     Maximises the maximum intensity of the real part of the echo.
     """
-    return -np.max(data.O.imag)
+    return -minimagint_echo(data)
+
+
+def minabsmax_echo(data):
+    """
+    Minimises the absolute (magnitude-mode) maximum of the echo.
+    """
+    return -np.max(np.abs(data.O.real + 1j * data.O.imag))
+
+
+def maxabsmax_echo(data):
+    """
+    Maximises the absolute (magnitude-mode) maximum of the echo.
+    """
+    return -minabsmax_echo(data)
+
+
+def minrealmax_echo(data):
+    """
+    Minimises the maximum of the real part of the echo.
+    """
+    return np.max(data.O.real)
+
+
+def maxrealmax_echo(data):
+    """
+    Maximises the maximum of the real part of the echo.
+    """
+    return -minrealmax_echo(data)
+
+
+def minimagmax_echo(data):
+    """
+    Minimises the maximum of the imaginary part of the echo.
+    """
+    return np.max(data.O.imag)
+
+
+def maximagmax_echo(data):
+    """
+    Maximises the maximum of the imaginary part of the echo.
+    """
+    return -minimagmax_echo(data)
 
 
 # DEER trace modulation depth
 def max_n2p(data):
     """
-    maximize n2p parameter for DEER trace
-    data should contain the 2 points of interest in position 0 and 1.
+    Maximizes n2p parameter for DEER trace
+    Data should contain the 2 points of interest in position 0 and 1.
     """
     return -np.abs(data.O.real[0]-data.O.real[1])
