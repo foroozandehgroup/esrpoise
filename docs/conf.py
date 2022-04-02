525cd91979f0537076826b4a7d993d040f4e9eef
Jonathan Yong <yongrenjie@gmail.com>
2022-04-02 14:09:51 +0100

    silence warnings

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ rst_prolog = """
 default_role = "any"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -78,7 +78,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_templates']
+# html_static_path = ['_templates']
 
 html_theme_options = {
     "page_width": "1000px",
